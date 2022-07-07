0cd2ba41a2e3ede012e5e3d1af448f35cae52600
otarbakh <otarbakh@gmail.com>
2022-07-08 00:01:43 +0400

    final

--- app/src/main/java/com/example/hmwrk8/MainActivity.kt
+++ app/src/main/java/com/example/hmwrk8/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.hmwrk8
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log.d
+import com.example.hmwrk8.databinding.GetstartedBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: GetstartedBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.getstarted)
+        binding = GetstartedBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnGetstarted.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java).apply {
+            }
+            startActivity(intent)
+            finish()
+        }
     }
+//    override fun onPause() {
+//        super.onPause()
+//        d("methodName","onPause - დაპაუზდა  ექთივითი") // დაპაუზება
+//    }
+//
+//    override fun onStart() {
+//        super.onStart()
+//        d("methodName","onStart - ჩაიტვირთა ექთივითი") // დაწყება ხდება მაშნაც როცა ეკრანი ჩაქვრება და მერე აანთებ ისევ
+//    }
+//
+//    override fun onStop() {
+//        super.onStop()
+//        d("methodName","onStop - გაჩერდა აპლიკაცია") // გაჩერება ხდება როცა დაიბლოკება ეკრანი, ან ჩაქვრება, ან ხელით აწევ და წაშლი რესენთებიდან
+//    }
+//
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        d("methodName","onDesroy - განადგურდა აპი")
+//    }
 }
--- app/src/main/java/com/example/hmwrk8/MainActivity2.kt
+++ app/src/main/java/com/example/hmwrk8/MainActivity2.kt
@@ -2,10 +2,16 @@ package com.example.hmwrk8
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hmwrk8.databinding.GetstartedBinding
+import com.example.hmwrk8.databinding.SecondactcivityBinding
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: SecondactcivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.secondactcivity)
+        binding = SecondactcivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
